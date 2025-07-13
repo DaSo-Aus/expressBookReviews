2c8c3506d5e6c1c923f2476971f7644c818f0b92
DaSo-Aus <david_sommerkorn@outlook.de>
2025-07-13 11:51:01 +0200

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,33 +11,73 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function (req, res) {
+  return res.status(200).send(JSON.stringify(books, null, 4));
 });
+
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+  
+    if (book) {
+      return res.status(200).json(book);
+    } else {
+      return res.status(404).json({ message: "Book not found" });
+    }
+  });
+  
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author;
+    const matchingBooks = [];
+  
+    for (let key in books) {
+      if (books[key].author.toLowerCase() === author.toLowerCase()) {
+        matchingBooks.push(books[key]);
+      }
+    }
+  
+    if (matchingBooks.length > 0) {
+      return res.status(200).json(matchingBooks);
+    } else {
+      return res.status(404).json({ message: "No books found for this author" });
+    }
+  });
+  
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/title/:title', function (req, res) {
+    const title = req.params.title.toLowerCase();
+    const matchingBooks = [];
+  
+    for (let key in books) {
+      if (books[key].title.toLowerCase() === title) {
+        matchingBooks.push(books[key]);
+      }
+    }
+  
+    if (matchingBooks.length > 0) {
+      return res.status(200).json(matchingBooks);
+    } else {
+      return res.status(404).json({ message: "No books found with this title" });
+    }
+  });
+  
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/review/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+  
+    if (book && book.reviews) {
+      return res.status(200).json(book.reviews);
+    } else {
+      return res.status(404).json({ message: "Reviews not found for this ISBN" });
+    }
+  });
+  
 
 module.exports.general = public_users;
